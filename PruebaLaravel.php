f3b6f66389717bf8262440e590c1ccb7610faf99
tonyblaze502 <insspira502@gmail.com>
2018-08-21 01:12:33 -0600

    Pruebas unitarias laravel
    Continuan las pruebas unitarias, sigo documentandome para hacerlo de manera correcta.

---
+++ PruebaLaravel.php
@@ -0,0 +1,19 @@
+<?php
+
+Class PruebaLaravel{
+
+public function __construct(){
+
+}
+
+public function suma($a,$b){
+   return $a+$b;
+}
+
+public function multiplicacion($a,$b){
+   return $a*$b;
+}
+
+}
+
+?>
