cb5435bc631364d3cac2e808ff04bcbd36d3f320
Julia <julyargesh@gmail.com>
2018-09-09 13:13:42 -0600

    Programacion en parejas 201503865-201513629

---
+++ MOBILE/www/build/0.js
@@ -0,0 +1,87 @@
+webpackJsonp([0],{
+
+/***/ 331:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ProfesorListadoJuegosPageModule", function() { return ProfesorListadoJuegosPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__profesor_listado_juegos__ = __webpack_require__(347);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var ProfesorListadoJuegosPageModule = (function () {
+    function ProfesorListadoJuegosPageModule() {
+    }
+    return ProfesorListadoJuegosPageModule;
+}());
+ProfesorListadoJuegosPageModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__profesor_listado_juegos__["a" /* ProfesorListadoJuegosPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__profesor_listado_juegos__["a" /* ProfesorListadoJuegosPage */]),
+        ],
+    })
+], ProfesorListadoJuegosPageModule);
+
+//# sourceMappingURL=profesor-listado-juegos.module.js.map
+
+/***/ }),
+
+/***/ 347:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ProfesorListadoJuegosPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(4);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+/**
+ * Generated class for the ProfesorListadoJuegosPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+var ProfesorListadoJuegosPage = (function () {
+    function ProfesorListadoJuegosPage(navCtrl, navParams) {
+        this.navCtrl = navCtrl;
+        this.navParams = navParams;
+    }
+    ProfesorListadoJuegosPage.prototype.ionViewDidLoad = function () {
+        console.log('ionViewDidLoad ProfesorListadoJuegosPage');
+    };
+    return ProfesorListadoJuegosPage;
+}());
+ProfesorListadoJuegosPage = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'page-profesor-listado-juegos',template:/*ion-inline-start:"C:\Users\ASUS\Documents\GitHub\Grupo4\MOBILE\src\pages\_profesor\profesor-listado-juegos\profesor-listado-juegos.html"*/'<!--\n\n  Generated template for the ProfesorListadoJuegosPage page.\n\n\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n\n  Ionic pages and navigation.\n\n-->\n\n<ion-header>\n\n\n\n  <ion-navbar>\n\n    <ion-title>profesor-listado-juegos</ion-title>\n\n  </ion-navbar>\n\n\n\n</ion-header>\n\n\n\n\n\n<ion-content padding>\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\ASUS\Documents\GitHub\Grupo4\MOBILE\src\pages\_profesor\profesor-listado-juegos\profesor-listado-juegos.html"*/,
+    }),
+    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* NavParams */]])
+], ProfesorListadoJuegosPage);
+
+//# sourceMappingURL=profesor-listado-juegos.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
