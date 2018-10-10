f5a2d289b641c0ec8a385cb723ffb94cb0217de1
Unknown <insspira502@gmail.com>
2018-10-09 22:26:57 -0600

    TDD_Fase4

--- Backend/Laravel/Proyecto_G4/tests/Feature/ControllerTest.php
+++ Backend/Laravel/Proyecto_G4/tests/Feature/ControllerTest.php
@@ -190,13 +190,106 @@ class ExampleTest extends TestCase
                 ]);
 
 
+    }
 
+/*pruebas a la base de datos Fase 4*/
+public function testDevolver_mas_cursos()
+    {      
+         $response = $this->get('Devolver_Cursos');
+         $this->assertDatabaseHas('Curso', [
+        'Nombre' => 'Curso1'
+    ]);
+       
+       $response = $this->get('Devolver_Cursos');
+         $this->assertDatabaseHas('Curso', [
+        'Nombre' => 'Curso2'
+    ]);
+
+         $response = $this->get('Devolver_Cursos');
+         $this->assertDatabaseHas('Curso', [
+        'Nombre' => 'Curso3'
+    ]);
+
+         $response = $this->get('Devolver_Cursos');
+         $this->assertDatabaseHas('Curso', [
+        'Nombre' => 'Curso4'
+    ]);
+         $response = $this->get('Devolver_Cursos');
+         $this->assertDatabaseHas('Curso', [
+        'Nombre' => 'Curso5'
+    ]);
 
 
 
     }
 
 
+public function testInsertar_mas_Boletines()
+    {
+         
+        $response = $this->get('InsertarBoletin');
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '1',
+                'idCurso' => '1'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '2',
+                'idCurso' => '2'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '3',
+                'idCurso' => '3'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '4',
+                'idCurso' => '4'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '5',
+                'idCurso' => '5'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '6',
+                'idCurso' => '6'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '7',
+                'idCurso' => '7'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '8',
+                'idCurso' => '8'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '9',
+                'idCurso' => '9'
+                ]);
+        $this->assertDatabaseHas('Boletin', [
+                'Ttitulo' => 'Dia del padre',
+                'Informacion' => 'Se hara un festejo',
+                'idCatedratico' => '10',
+                'idCurso' => '10'
+                ]);
 
+
+    }
 
 }
