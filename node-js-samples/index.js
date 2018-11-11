d9a7660bfe7c1a3abcdfc48e2cfc7594b2184dde
gamboa <holagamboatavo@gmail.com>
2018-11-10 18:52:12 -0600

    nr

--- node-js-samples/index.js
+++ node-js-samples/index.js
@@ -4,34 +4,30 @@ var mysql      = require('mysql');
 
 
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'mydb3.cuzllkjwjtyt.us-east-1.rds.amazonaws.com',
-  user     : 'admin',
-  password : 'Seminario1234'
-});
 
 
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-  
-    console.log('connected as id ' + connection.threadId);
-    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-      if (error) throw error;
-      console.log('The solution is: ', results[0].solution);
-    });
-    
-    connection.end();
 
-  });
+var connection = mysql.createConnection({
+  host     : 'seminariords.cmm07rfgsbfs.us-east-2.rds.amazonaws.com',
+  user     : 'admin',
+  password : 'Seminario1234',
+  database : 'mydb'
+});
+
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
+
 
   response.send('Hello World!')
 })
@@ -39,3 +35,4 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
